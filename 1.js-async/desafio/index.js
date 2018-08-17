a5e67cb6b432b8b6d27f8e132072548f8416648d
Erick Wendel <erick.workspace@gmail.com>
2018-08-17 19:06:57 -0300

    exemplos
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- 1.js-async/desafio/index.js
+++ 1.js-async/desafio/index.js
@@ -10,6 +10,35 @@
 // importamos a biblioteca para fazer requisicoes externas
 // const request = require('request')
 
+/**
+ * 
+
+ https://swapi.co/api/people/?search=luke&format=json
+
+http://dontpad.com/resolvstudygroup
+
+- Consumir API do swapi.co
+- Obter Pessoas pelo nome
+- A partir da pessoa recebida, pegar também Starship Instance
+
+{
+    "gender": "Male",
+    "hair_color": "Blond",
+    "height": "172",
+    "homeworld": "https://swapi.co/api/planets/1/",
+    "mass": "77",
+    "name": "Luke Skywalker",
+    "skin_color": "Fair",
+    "created": "2014-12-09T13:50:51.644000Z",
+    "edited": "2014-12-10T13:52:43.172000Z",
+    "starship": {
+        "name": "X-wing", 
+        "model": "T-65 X-wing", 
+    }
+}
+
+
+ */
 
 const util = require('util')
 const request = require('request')
